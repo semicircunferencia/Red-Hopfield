8b52037d523e294a18bb75d136fcffd66aef4b0d
semicircunferencia <125926149+semicircunferencia@users.noreply.github.com>
2023-05-28 19:25:43 +0200

    l

--- Scripts Python/grafica_solapvsT.py
+++ Scripts Python/grafica_solapvsT.py
@@ -8,14 +8,14 @@ ax = plt.subplot()      # subfigura
 
 # Datos
 data=[0]*8
-#data1=[0]*8
-#data2=[0]*8
-#data3=[0]*8
+data1=[0]*8
+data2=[0]*8
+data3=[0]*8
 for i in range(8):
     data[i]=np.loadtxt(f"T={i+1}.dat")
-    data[i]=abs(np.mean(data[i][70:101]))
-    #data2[i]=abs(np.mean(data[i][70:101,1]))
-    #data3[i]=abs(np.mean(data[i][70:101,2]))
+    data1[i]=abs(np.mean(data[i][70:101,0]))
+    data2[i]=abs(np.mean(data[i][70:101,1]))
+    data3[i]=abs(np.mean(data[i][70:101,2]))
 
 temperatura = [1e-4, 1e-3, 1e-2, 2e-2, 3e-2, 4e-2, 5e-2, 1e-1]
 
@@ -32,11 +32,11 @@ ax.set_xlabel('Temperatura', fontname='DejaVu Sans', fontsize='12')
 #plt.yticks(fontsize='15')
 
 # Creación de la gráfica
-ax.plot(temperatura, data, linestyle='-', marker='', markersize=4, color='#B4045F', label="XANA", linewidth=1.0)
-#ax.plot(temperatura, data2, linestyle='-', marker='', markersize=4, color='#5FB404', label="Luna", linewidth=1.0)
-#ax.plot(temperatura, data3, linestyle='-', marker='', markersize=4, color='#045FB4', label="Burbuja", linewidth=1.0)
+ax.plot(temperatura, data1, linestyle='-', marker='.', markersize=4, color='#B4045F', label="XANA", linewidth=1.0)
+ax.plot(temperatura, data2, linestyle='-', marker='.', markersize=4, color='#5FB404', label="Luna", linewidth=1.0)
+ax.plot(temperatura, data3, linestyle='-', marker='.', markersize=4, color='#045FB4', label="Burbuja", linewidth=1.0)
 
-#plt.legend()
+plt.legend()
 
 
 # Guardar la gráfica
